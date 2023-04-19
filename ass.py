f48a674bbf0b383a69e8a73830f76cdc0e1ccf55
sunil1982 <sksunilsaini4@gmail.com>
2023-04-19 22:24:55 +0530

    Update ass.py

--- ass.py
+++ ass.py
@@ -10,9 +10,9 @@ def main():
     st.write('Enter three number and then click on the buutton to find the largest')
     
 
-num1 = float(st.number_input("Enter first number: "))
-num2 = float(st.number_input("Enter second number: "))
-num3 = float(st.number_input("Enter third number: "))
+num1 = st.number_input("Enter first number: ")
+num2 = st.number_input("Enter second number: ")
+num3 = st.number_input("Enter third number: ")
 largest_num=0
 
 
